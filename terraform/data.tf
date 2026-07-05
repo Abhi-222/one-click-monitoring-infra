751421ee8671009593db75b1ac5f8189625e4175
sahil <sahil@example.com>
2026-07-05 21:39:23 +0000

    ninteenth commit

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,12 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# Dynamically fetches the current, verified AL2023 AMI ID directly from AWS
-data "aws_ssm_parameter" "al2023_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-x86_64" # or "al2023-ami-kernel-default-x86_64"
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
